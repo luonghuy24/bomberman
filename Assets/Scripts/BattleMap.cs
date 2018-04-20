d2c5d68940c87c61abdcb2229a3ce93476a85bb4
luonghuy24 <mrluonghuy@gmail.com>
2018-04-20 05:24:08 -0400

    auto generate detructable objects

--- Assets/Scripts/BattleMap.cs
+++ Assets/Scripts/BattleMap.cs
@@ -8,9 +8,40 @@ public class BattleMap : NetworkBehaviour {
 	[SerializeField]
 	private GameObject boxPrefab;
 
+
+	private List<Vector2> boxPositions;
+
 	// Use this for initialization
 	void Start () {
 		if (isServer) {
+
+			boxPositions = new List<Vector2>();
+
+			for (int y = 1; y > -8; y = y - 2) {				
+				for (int x = -7; x < 6; x = x + 1) {	
+					Vector2 p = new Vector2 (x, y);
+					boxPositions.Add (p);
+				}
+			}
+
+			for (int y = 0; y > -7; y = y - 2) {				
+				for (int x = -7; x < 6; x = x + 2) {	
+					Vector2 p = new Vector2 (x, y);
+					boxPositions.Add (p);
+				}
+			}
+				
+			for (int i = -5; i < 4; i++) {				
+				boxPositions.Add (new Vector2 (i, 3));
+				boxPositions.Add (new Vector2 (i, -9));
+			}
+
+			for (int i = -5; i < 4; i=i+2) {				
+				boxPositions.Add (new Vector2 (i, 2));
+				boxPositions.Add (new Vector2 (i, -8));
+			}
+
+
 			CmdGenerateBoxes ();
 		} else {
 			Debug.Log("I'm the client");
@@ -20,15 +51,20 @@ public class BattleMap : NetworkBehaviour {
 	[Command]
 	void CmdGenerateBoxes() {
 		if (NetworkServer.active) {
-			GameObject box = Instantiate(boxPrefab, new Vector3(Mathf.RoundToInt(1), 
-				Mathf.RoundToInt(2), 0),
-				Quaternion.identity);
-			NetworkServer.Spawn (box);
 
-			GameObject box1 = Instantiate(boxPrefab, new Vector3(Mathf.RoundToInt(1), 
-				Mathf.RoundToInt(-2), 0),
-				Quaternion.identity);
-			NetworkServer.Spawn (box1);
+			for (int i = 0; i < 100; i++) {
+
+				int index = Random.Range (0, boxPositions.Count);
+
+				Vector2 v = boxPositions [index];
+			
+				GameObject box = Instantiate(boxPrefab, new Vector3(Mathf.RoundToInt(v.x), 
+					Mathf.RoundToInt(v.y), 0),
+					Quaternion.identity);
+				NetworkServer.Spawn (box);
+
+				boxPositions.Remove (v);
+			}
 		}
 	}
 }
