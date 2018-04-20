f0549d94ce7e2d1d6838f1356ca8130da85ca618
luonghuy24 <mrluonghuy@gmail.com>
2018-04-20 00:05:52 -0400

    refine

--- Assets/Scripts/ExplosionDamage.cs
+++ Assets/Scripts/ExplosionDamage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExplosionDamage : MonoBehaviour {
 
-	void OnTriggerEnter2D(Collider2D collider) {
+	void OnTriggerEnter2D(Collider2D collider) {		
 		if (collider.tag == "Character") {
 			collider.gameObject.GetComponent<PlayerLife> ().LoseLife ();
 		} else if (collider.tag == "Bomb") {
